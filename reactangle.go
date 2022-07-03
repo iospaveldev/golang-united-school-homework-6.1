652c8b7fce35c9c707d500d14a68e3d383a36deb
Pavel Sakhanko <iospaveldev@gmail.com>
2022-07-03 20:04:09 +0200

    fix

--- reactangle.go
+++ reactangle.go
@@ -6,7 +6,7 @@ type Rectangle struct {
 }
 
 func (r Rectangle) CalcPerimeter() float64 {
-	return 2*r.Weight + 2*r.Weight
+	return 2*r.Weight + 2*r.Height
 }
 
 func (r Rectangle) CalcArea() float64 {
